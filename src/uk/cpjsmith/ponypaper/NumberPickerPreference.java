19527df3ebb68a82a5404aaad3c6b0e32195a78e
Chris Smith <chrissmithers888@gmail.com>
2015-09-06 11:51:16 +0100

    Show the current number picker value on the preferences screen. Also various code style changes in NumberPickerPreference.java.

--- src/uk/cpjsmith/ponypaper/NumberPickerPreference.java
+++ src/uk/cpjsmith/ponypaper/NumberPickerPreference.java
@@ -10,55 +10,55 @@ import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.NumberPicker;
 
-import android.util.Log;
-
 /**
  * A {@link android.preference.Preference} that displays a number picker as a dialog.
  */
 public class NumberPickerPreference extends DialogPreference {
-
+    
+    private static final String XMLNS_CUSTOM = "http://cpjsmith.uk/ponypaper/custom";
+    
     // allowed range
-    public static final int DEFAULT_MAX_VALUE = 10;
-    public static final int DEFAULT_MIN_VALUE = 1;
+    private static final int DEFAULT_MAX_VALUE = 10;
+    private static final int DEFAULT_MIN_VALUE = 1;
     // enable or disable the 'circular behavior'
-    public static final boolean DEFAULT_WRAP_SELECTOR_WHEEL = true; 
-
+    private static final boolean DEFAULT_WRAP_SELECTOR_WHEEL = true;
+    
     private NumberPicker picker;
     private int value;
-
+    
     private int maxValue;
     private int minValue;
     private boolean wrapSelectorWheel;
-
+    
     public NumberPickerPreference(Context context, AttributeSet attrs) {
         super(context, attrs);
-        maxValue = attrs.getAttributeIntValue("http://cpjsmith.uk/ponypaper/custom", "maxValue", DEFAULT_MAX_VALUE);
-        minValue = attrs.getAttributeIntValue("http://cpjsmith.uk/ponypaper/custom", "minValue", DEFAULT_MIN_VALUE);
-        wrapSelectorWheel = attrs.getAttributeBooleanValue("http://cpjsmith.uk/ponypaper/custom", "wrap", DEFAULT_WRAP_SELECTOR_WHEEL);
+        maxValue = attrs.getAttributeIntValue(XMLNS_CUSTOM, "maxValue", DEFAULT_MAX_VALUE);
+        minValue = attrs.getAttributeIntValue(XMLNS_CUSTOM, "minValue", DEFAULT_MIN_VALUE);
+        wrapSelectorWheel = attrs.getAttributeBooleanValue(XMLNS_CUSTOM, "wrap", DEFAULT_WRAP_SELECTOR_WHEEL);
     }
-
+    
     public NumberPickerPreference(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        maxValue = attrs.getAttributeIntValue("http://cpjsmith.uk/ponypaper/custom", "maxValue", DEFAULT_MAX_VALUE);
-        minValue = attrs.getAttributeIntValue("http://cpjsmith.uk/ponypaper/custom", "minValue", DEFAULT_MIN_VALUE);
-        wrapSelectorWheel = attrs.getAttributeBooleanValue("http://cpjsmith.uk/ponypaper/custom", "wrap", DEFAULT_WRAP_SELECTOR_WHEEL);
+        maxValue = attrs.getAttributeIntValue(XMLNS_CUSTOM, "maxValue", DEFAULT_MAX_VALUE);
+        minValue = attrs.getAttributeIntValue(XMLNS_CUSTOM, "minValue", DEFAULT_MIN_VALUE);
+        wrapSelectorWheel = attrs.getAttributeBooleanValue(XMLNS_CUSTOM, "wrap", DEFAULT_WRAP_SELECTOR_WHEEL);
     }
-
+    
     @Override
     protected View onCreateDialogView() {
-        FrameLayout.LayoutParams layoutParams = new FrameLayout.LayoutParams(
-                ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        FrameLayout.LayoutParams layoutParams = new FrameLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
+                                                                             ViewGroup.LayoutParams.WRAP_CONTENT);
         layoutParams.gravity = Gravity.CENTER;
-
+        
         picker = new NumberPicker(getContext());
         picker.setLayoutParams(layoutParams);
-
+        
         FrameLayout dialogView = new FrameLayout(getContext());
         dialogView.addView(picker);
-
+        
         return dialogView;
     }
-
+    
     @Override
     protected void onBindDialogView(View view) {
         super.onBindDialogView(view);
@@ -67,7 +67,7 @@ public class NumberPickerPreference extends DialogPreference {
         picker.setWrapSelectorWheel(wrapSelectorWheel);
         picker.setValue(getValue());
     }
-
+    
     @Override
     protected void onDialogClosed(boolean positiveResult) {
         if (positiveResult) {
@@ -77,23 +77,25 @@ public class NumberPickerPreference extends DialogPreference {
             }
         }
     }
-
+    
     @Override
     protected Object onGetDefaultValue(TypedArray a, int index) {
         return a.getInt(index, minValue);
     }
-
+    
     @Override
     protected void onSetInitialValue(boolean restorePersistedValue, Object defaultValue) {
         setValue(restorePersistedValue ? getPersistedInt(minValue) : (Integer) defaultValue);
     }
-
-    public void setValue(int value) {
-        this.value = value;
-        persistInt(this.value);
+    
+    private void setValue(int newValue) {
+        value = newValue;
+        persistInt(value);
+        setSummary(Integer.toString(value));
     }
-
-    public int getValue() {
-        return this.value;
+    
+    private int getValue() {
+        return value;
     }
+    
 }
